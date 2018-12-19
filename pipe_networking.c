37ee06714b0a84c6e0ef1052b8f56069ff53aff9
Mandy <mmai1@stuy.edu>
2018-12-19 06:58:59 -0500

    update

--- pipe_networking.c
+++ pipe_networking.c
@@ -11,7 +11,38 @@
   returns the file descriptor for the upstream pipe.
   =========================*/
 int server_handshake(int *to_client) {
-  return 0;
+  int fd;
+  if(mkfifo("wkp", 0644) == -1){
+    printf("mkfifo Error: %s\n", strerror(errno));
+    exit(0);
+  }
+  printf("WKP server FIFO created...\n");
+
+  printf("Server waiting for client...\n");
+  fd = open("wkp", O_RDONLY);
+  char buf[HANDSHAKE_BUFFER_SIZE];
+  read(fd, buf, HANDSHAKE_BUFFER_SIZE);
+  printf("Server has received message from client...\n");
+  printf("\"%s\"\n", buf);
+
+  *to_client = open(buf, O_WRONLY);
+  remove("wkp");
+  printf("Server has removed WKP and connected to client...\n");
+
+  write(*to_client, ACK, HANDSHAKE_BUFFER_SIZE);
+  printf("Server has sent initial acknowledgement message to client...\n");
+
+  read(fd, buf, HANDSHAKE_BUFFER_SIZE);
+  printf("Server has received response from client...\n");
+  printf("\"%s\"\n", buf);
+
+  close(*to_client);
+  free(buf);
+  close(fd);
+  printf("Server has closed all connections to client...\n");
+  printf("Server is waiting for new client...\n");
+
+  return fd;
 }
 
 
@@ -25,5 +56,31 @@ int server_handshake(int *to_client) {
   returns the file descriptor for the downstream pipe.
   =========================*/
 int client_handshake(int *to_server) {
-  return 0;
+  int fd;
+  if(mkfifo("private", 0644) == -1){
+    printf("mkfifo Error: %s\n", strerror(errno));
+    exit(0);
+  }
+  printf("Private client FIFO created...\n");
+
+  *to_server = open("private", O_WRONLY);
+  write(*to_server, "client", BUFFER_SIZE);
+  printf("Client has connected to server and sent private FIFO name...\n");
+
+  fd = open("private", O_RDONLY);
+  char buf[HANDSHAKE_BUFFER_SIZE];
+  read(fd, buf, HANDSHAKE_BUFFER_SIZE);
+  printf("Client has received message from server and removed its private FIFO...\n");
+  printf("\"%s\"\n", buf);
+  remove("private");
+
+  write(*to_server, ACK, HANDSHAKE_BUFFER_SIZE);
+  printf("Client has sent response to server...\n");
+
+  close(*to_server);
+  free(buf);
+  close(fd);
+  printf("Client has exited...\n");
+
+  return fd;
 }
