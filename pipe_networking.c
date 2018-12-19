0c7f43e11fa502425e977bea4a5d4fcff33e821d
Mandy <mmai1@stuy.edu>
2018-12-19 14:30:56 -0500

    update

--- pipe_networking.c
+++ pipe_networking.c
@@ -11,22 +11,23 @@
   returns the file descriptor for the upstream pipe.
   =========================*/
 int server_handshake(int *to_client) {
+  remove("server");
   int fd;
-  if(mkfifo("wkp", 0644) == -1){
-    printf("mkfifo Error: %s\n", strerror(errno));
+  if(mkfifo("server", 0644) == -1){
+    printf("mkfifo error: %s\n", strerror(errno));
     exit(0);
   }
   printf("WKP server FIFO created...\n");
 
   printf("Server waiting for client...\n");
-  fd = open("wkp", O_RDONLY);
+  fd = open("server", O_RDONLY);
   char buf[HANDSHAKE_BUFFER_SIZE];
   read(fd, buf, HANDSHAKE_BUFFER_SIZE);
   printf("Server has received message from client...\n");
   printf("\"%s\"\n", buf);
 
   *to_client = open(buf, O_WRONLY);
-  remove("wkp");
+  // remove("server");
   printf("Server has removed WKP and connected to client...\n");
 
   write(*to_client, ACK, HANDSHAKE_BUFFER_SIZE);
@@ -56,23 +57,24 @@ int server_handshake(int *to_client) {
   returns the file descriptor for the downstream pipe.
   =========================*/
 int client_handshake(int *to_server) {
+  remove("client");
   int fd;
-  if(mkfifo("private", 0644) == -1){
-    printf("mkfifo Error: %s\n", strerror(errno));
+  if(mkfifo("client", 0644) == -1){
+    printf("mkfifo error: %s\n", strerror(errno));
     exit(0);
   }
   printf("Private client FIFO created...\n");
 
-  *to_server = open("private", O_WRONLY);
+  *to_server = open("client", O_WRONLY);
   write(*to_server, "client", BUFFER_SIZE);
   printf("Client has connected to server and sent private FIFO name...\n");
 
-  fd = open("private", O_RDONLY);
+  fd = open("client", O_RDONLY);
   char buf[HANDSHAKE_BUFFER_SIZE];
   read(fd, buf, HANDSHAKE_BUFFER_SIZE);
   printf("Client has received message from server and removed its private FIFO...\n");
   printf("\"%s\"\n", buf);
-  remove("private");
+  // remove("client");
 
   write(*to_server, ACK, HANDSHAKE_BUFFER_SIZE);
   printf("Client has sent response to server...\n");
