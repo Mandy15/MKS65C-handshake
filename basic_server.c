41d550943066e73dc4d53e7c5a611d0c7f822997
Mandy <mmai1@stuy.edu>
2018-12-20 14:39:31 -0500

    update

--- basic_server.c
+++ basic_server.c
@@ -1,10 +1,33 @@
 #include "pipe_networking.h"
 
+static void sighandler(int signo){
+  if(signo == SIGINT){
+    remove("server");
+    printf("\nServer has exited.\n");
+    exit(0);
+  }
+}
 
 int main() {
+  signal(SIGINT, sighandler);
 
   int to_client;
   int from_client;
 
   from_client = server_handshake( &to_client );
+
+  int i;
+  char * buf = malloc(BUFFER_SIZE*sizeof(char));
+
+  while(read(from_client, buf, BUFFER_SIZE)){
+    printf("Server has received input from client...processing data...\n");
+    printf("\"%s\"\n", buf);
+    for(i = 0; i < strlen(buf); i++){
+      buf[i] += 13;
+    }
+
+    write(to_client, buf, BUFFER_SIZE);
+    printf("Server has sent data back to client...\n");
+  }
+  printf("Client has disconnected. Reset for new handshake.\n");
 }
--- client.c
+++ client.c
@@ -1,10 +1,28 @@
 #include "pipe_networking.h"
 
+static void sighandler(int signo){
+  if(signo == SIGINT){
+    printf("\nClient has exited.\n");
+    exit(0);
+  }
+}
 
 int main() {
+  signal(SIGINT, sighandler);
 
   int to_server;
   int from_server;
 
   from_server = client_handshake( &to_server );
+
+  char * buf = malloc(BUFFER_SIZE*sizeof(char));
+  while(1){
+    printf("Enter input to send to server: ");
+    fgets(buf, BUFFER_SIZE, stdin);
+    write(to_server, buf, BUFFER_SIZE);
+
+    read(from_server, buf, BUFFER_SIZE);
+    printf("Client has received message from server...\n");
+    printf("\"%s\"\n", buf);
+  }
 }
